5ced3326c7854bd9caa96ef039622748b685c68a
Yevhen L <yevhen.lozovatslyi@gmail.com>
2022-09-27 09:23:46 +0300

    Code upadate

--- hw3.py
+++ hw3.py
@@ -1,4 +1,4 @@
-# 1. Зформуйте строку, яка містить певну інформацію про символ в відомому слові.
+# 1. Сформуйте строку, яка містить певну інформацію про символ в відомому слові.
 # Наприклад "The [номер символу] symbol in [тут слово] is '[символ з відповідним порядковим номером]'".
 # Слово та номер отримайте за допомогою input() або скористайтеся константою.
 # Наприклад (слово - "Python" а номер символу 3) - "The 3 symbol in "Python" is 't' ".
@@ -13,21 +13,21 @@ while True:
     except ValueError as e:
         print('Error! Enter int!')
     else:
-     if 0 < user_idx <= len(user_word):
-        print(f' The {user_idx} symbol in {user_word} is {user_word[user_idx - 1]}')
-     else:
-        print('Error! Index out of range!')
-        if counter > 7:
-         break
+        if 0 < user_idx <= len(user_word):
+            print(f' The {user_idx} symbol in {user_word} is {user_word[user_idx - 1]}')
+        else:
+            print('Error! Index is out of range!')
+    if counter > 7:
+        break
 
 
 # 2. Написати цикл, який буде вимагати від користувача ввести слово, в якому є буква "о"
-# (враховуються як великі так і маленькі).
+# (враховуються як великі, так і маленькі).
 # Цикл не повинен завершитися, якщо користувач ввів слово без букви о.
 while True:
     userinput = input('Enter the word with letter o:')
     if userinput.__contains__('o') or userinput.__contains__('O'):
-        print('Thnx!')
+        print('Thank you! The word with "o" is entered!')
         break
     else:
-     print('You entered word without letter "o"!')
+        print('You entered word without letter "o"!')
